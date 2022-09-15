1d2959d90c9d4855134bc4b8eb154054faa147b8
Denu <Denu@dota2.ru>
2022-09-15 15:09:03 +0300

    дабовил вывод текстов

--- controllers/texts.controller.js
+++ controllers/texts.controller.js
@@ -3,7 +3,7 @@ const Texts = require("../models/texts.model");
 module.exports.textController = {
   postTexts: async (req, res) => {
     try {
-      const {name, title, time, price, point } = req.body;
+      const {name, title, time, price, point, count, completed } = req.body;
 
       const textic = await Texts.create({
         name,
@@ -11,12 +11,23 @@ module.exports.textController = {
         time,
         price,
         point,
+        count,
+        completed
       });
-
       return res.json(textic);
     } catch (error) {
       return res.status(400).json({
         error: "Ошибка при создании заметки: " + error.toString(),
+      });
+    }
+  },
+  getTextsById: async (req, res) => {
+    try {
+      const textsic = await Texts.findById(req.params.id);
+      return res.json(textsic);
+    } catch (error) {
+      return res.status(400).json({
+        error: "Ошибка при выводе текста по ID: " + error.message,
       });
     }
   },
